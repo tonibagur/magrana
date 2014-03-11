6fa0abd1c00669871f12b1c555133c6ee8287548
Coneptum <coneptum@coneptum-VirtualBox.(none)>
2014-03-11 12:58:23 +0100

    add PomeloUser and Gift in adminitration of users

--- pomeloUsers/admin.py
+++ pomeloUsers/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from pomeloUser.models import PomeloUser, Gift
 
-# Register your models here.
+class PomeloUserAdmin(admin.ModelAdmin):
+	list_display = ('id', 'user')
+	
+class GiftAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'price', 'url_video', 'sender', 'receiver')
+	
+admin.siste.register(PomeloUser, PomeloUserAdmin)
+admin.siste.register(Gift, GiftAdmin)
