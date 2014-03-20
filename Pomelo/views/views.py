b0283b7162feea04aaa7a16cd61e4de82f1dd940
Coneptum <coneptum@coneptum-VirtualBox.(none)>
2014-03-20 11:00:28 +0100

    add botton browse in form with functionality

--- Pomelo/views/views.py
+++ Pomelo/views/views.py
@@ -3,51 +3,65 @@ from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.template import RequestContext, loader
-from .. forms.form import GiftForm
-from .. models.models import PomeloUser
-import random
 from django_youtube.views import upload 
 from django_youtube.models import Video
+from django.core.mail import send_mail
+import random
+from .. forms.form import GiftForm
+from .. models.models import PomeloUser
+#send_mail('Subject here', 'Here is the message.', 'from@example.com', ['to@example.com'], fail_silently=False)
+
+subject = 'You\'ve received a Pomelo Card'
+
+def create_message(name_sender, name_receiver, message_sender, url):
+    message = 'Dear ' + str(name_receiver) + ',\n' + name_sender  + ' just sent you a Pomelo Card Gift.\nCheck the video at the following url:\n' + url + '\n\n"' + message_sender + '" -- ' + name_sender + '\n\nPomelo\'s Team.'
+    return message
 
 def registerGift(request):
-    if (request.method=='POST'):
-        request_copy = request.POST.copy()
-        receiver = False
-        if (request.POST['receiver']=='' and request.POST['email']!= ''):
-            user = User.objects.create_user(request.POST['email'].split('@')[0], request.POST['email'], str(random.randrange(1000, 9000)))
-            receiver = PomeloUser(user=user)
-            receiver.save()
-            request_copy['receiver'] = str(receiver.id)
-        form = GiftForm(request_copy)
-        if form.is_valid():
-            gift = form.save(commit=False)
-            gift.sender = PomeloUser.objects.get(user=request.user.id)
-            gift.product = request.POST['products'] 
-            gift.save()	    
-            return render(request, "resposta.html")
-        else:
-            print 'errors',form.errors         
-            return render(request, "form.html", {
-                'form' : form,
-                'form_error': form.errors,
-            })
-    else:
-        form = False
-        video_id = request.GET.get('video_id', '')
-        if video_id:
-            videos = Video.objects.filter(video_id=str(video_id))
-            video = list(videos[:1])
-            if video:
-                form = GiftForm(initial={
-                    'url_video': video[0].youtube_url,
-                    'youtube_embed' : ('embed/').join(video[0].youtube_url.split('&feature')[0].split('watch?v=')),
-                    }) 
-        else:
-            form = GiftForm()
-        youtube_form,y_post_url,y_next_url = upload(request,ret_view=False) 
-        return render(request, "form.html", {
-                'form' : form,
-                'youtube_form': youtube_form,
-                'y_post_url':y_post_url,
-                'y_next_url':y_next_url,
-            })
+        if (request.method=='POST'):
+	    request_copy = request.POST.copy()
+	    receiver = False
+	    if (request.POST['receiver']=='' and request.POST['email']!= ''):
+		user = User.objects.create_user(request.POST['email'].split('@')[0], request.POST['email'], str(random.randrange(1000, 9000)))
+		receiver = PomeloUser(user=user)
+		receiver.save()
+		request_copy['receiver'] = str(receiver.id)
+	    form = GiftForm(request_copy)
+	    if form.is_valid():
+		gift = form.save(commit=False)
+		gift.sender = PomeloUser.objects.get(user=request.user.id)
+		gift.product = request.POST['products']
+		gift.save()
+		receiver = PomeloUser.objects.get(user=gift.receiver)
+		message = create_message(request.user.username, receiver, gift.message, gift.url_video)
+		send_mail(subject, message, request.user.email, [request.POST['email']], fail_silently=False)  
+		return render(request, "resposta.html")
+	    else:
+		print 'errors',form.errors         
+		return render(request, "form.html", {
+		    'form' : form,
+		    'form_error': form.errors,
+		})
+	else:
+	    form = False
+	    video_id = request.GET.get('video_id', '')
+	    if video_id:
+		videos = Video.objects.filter(video_id=str(video_id))
+		video = list(videos[:1])
+		if video:
+		    form = GiftForm(initial={
+			'url_video': video[0].youtube_url,
+			'youtube_embed' : ('embed/').join(video[0].youtube_url.split('&feature')[0].split('watch?v=')),
+			}) 
+	    else:
+		form = GiftForm()
+	    youtube_form,y_post_url,y_next_url = upload(request,ret_view=False) 
+	    return render(request, "form.html", {
+		    'form' : form,
+		    'youtube_form': youtube_form,
+		    'y_post_url':y_post_url,
+		    'y_next_url':y_next_url,
+		})
+
+
+        
